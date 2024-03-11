1acc41138dfb459692b9a3f66d0fcb389490118a
letfontanetti <124930034+letfontanetti@users.noreply.github.com>
2024-03-11 17:26:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/livros'
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id_livro = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(100))
+    autor = db.Column(db.String(254))
+    ano_publicacao = db.Column(db.Integer)
+
+@app.route('/')
+def index():
+    livros = Livro.query.all()
+    return render_template("cadastrolivros.html", outro=livros)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Livro')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    ano_publicacao = request.form['ano_publicacao']
+
+    livro = Livro.query.filter_by(titulo=titulo).first()
+    if livro:
+        flash('Livro já existente')
+        return redirect(url_for('novo'))
+
+    novo_livro = Livro(titulo=titulo, autor=autor, ano_publicacao=ano_publicacao)
+    db.session.add(novo_livro)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
